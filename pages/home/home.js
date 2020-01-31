daac279c94278aba42ecd7832ea03e51a2f28faa
ue4-fans-code521 <kongziliang714@163.com>
2020-01-31 12:20:45 +0800

    初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,44 @@
 Page({
-  
+  data: {
+    name: "iwen",
+    age: 20,
+    count: 0,
+    animals: [{
+        name: "pig",
+        color: "black",
+        age: 2
+      },
+      {
+        name: "cat",
+        color: "white",
+        age: 10
+      },
+      {
+        name: "dog",
+        color: "bk",
+        age: 5
+      },
+      {
+        name: "cow",
+        color: "brown",
+        age: 3
+      }
+    ]
+  },
+
+  addclk() {
+    // console.log("按键点击事件发生~~")
+    this.setData({
+      count: this.data.count + 1
+    })
+  },
+
+  subclk() {
+    // console.log("按键点击事件发生~~")
+    this.setData({
+      count: this.data.count - 1
+    })
+
+  },
+
 })
